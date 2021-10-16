83762c769959b365ec80a2fb16b550a56d9fe137
Logan-bobo <logan.bobofed@gmail.com>
2021-10-16 20:02:34 +0100

    core functionality

--- app/main.py
+++ app/main.py
@@ -4,11 +4,8 @@ import sqlite3
 import os.path
 
 # Datbase Name
-db_name = "contacts.db"
-
-#add logic to create database if not exists when program boots and create table if not continue and connect to the DB
-#if os.path.isfile(f'./{db_name}'):
-
+db_name = 'contacts.db'
+contacts_table = 'contacts'
 
 # Connect to database
 conn = sqlite3.connect(db_name)
@@ -18,12 +15,37 @@ cursor = conn.cursor()
 
 # Create Table
 cursor.execute(f""" 
-CREATE TABLE contacts (
-    first_name TEXT,
-    last_name TEXT,
-    mobile_number INTEGER,
-    email TEXT
+    CREATE TABLE {contacts_table} (
+        contact_id INTEGER,
+        first_name TEXT,
+        last_name TEXT,
+        mobile_number INTEGER,
+        email TEXT
 )""")
+
+# Add contact
+cursor.execute(f"""
+    INSERT INTO {contacts_table} VALUES(
+        1,
+        'logan', 
+        'cox',
+        01234567891,
+        'logan@lolz.com'
+)""")
+
+# Select contact inforamtion to the database
+data = cursor.execute(f"""
+    SELECT 
+        contact_id,
+        first_name, 
+        last_name, 
+        mobile_number, 
+        email 
+    FROM {contacts_table}
+""").fetchall()
+
+# Update a entry in the database
+cursor.execute(f"""UPDATE {contacts_table} SET first_name = 'BOB' WHERE contact_id = 1  """)
 
 # Commit our changes
 conn.commit()
